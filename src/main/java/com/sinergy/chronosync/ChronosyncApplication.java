92ead3a8f2f81afb2d97f58cddf2d753e460ed77
Kenan Fejzić <92314295+kfejzic1@users.noreply.github.com>
2024-10-07 22:09:34 +0200

    Initial commit

---
+++ src/main/java/com/sinergy/chronosync/ChronosyncApplication.java
@@ -0,0 +1,13 @@
+package com.sinergy.chronosync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosyncApplication.class, args);
+	}
+
+}
